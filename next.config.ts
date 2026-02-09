59c689df887385420343d63086a6ad44df4ec2de
Aldo Wagner <aldowagner78@gmail.com>
2026-02-08 21:37:15 -0300

    Revert "config: Configurar Next.js para GitHub Pages"
    This reverts commit 5fa751adf62b4147a804d4194ae89aaeb41d5864.

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: '/CPCE_gestion',
-  images: {
-    unoptimized: true,
-  },
+  /* config options here */
 };
 
 export default nextConfig;
